a6191e01e85d837fa2d24c04e57f47a64b222eb0
José Augusto <joseaugusto0408@gmail.com>
2022-02-24 08:09:14 -0300

    middlewares

--- src/index.js
+++ src/index.js
@@ -7,6 +7,18 @@ app.use(express.json());
 
 const customers = []; //banco de dados fake.
 
+function verifyIfExistsAccountCPF(request, response, next){
+  const {cpf} = request.headers;
+
+  const customer = customers.find(customer => customer.cpf === cpf);
+  
+  if(!customer){
+    return response.status(400).json({error: "Customer not found"});
+  }
+  request.customer = customer;
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
@@ -28,14 +40,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement/", (request, response) => {
-  const { cpf } = request.headers;
+app.get("/statement/", verifyIfExistsAccountCPF, (request, response) => {
 
-  const customer = customers.find((customer) => customer.cpf === cpf);
-
-  if(!customer){
-    return response.status(400).json({error: "Customer not found"});
-  }
+  const {customer} = request;
+  
   return response.status(201).json(customer.statement);
 });
 app.listen(3333);
