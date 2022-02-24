4445b04153d8422eda335d6936647ae2b3a26794
José Augusto <joseaugusto0408@gmail.com>
2022-02-24 07:50:54 -0300

    validate account

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ const { v4: uuidv4 } = require("uuid");
 const app = express();
 app.use(express.json());
 
-const customers = [];
+const customers = []; //banco de dados fake.
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
 
   const customersAlreadyExists = customers.some(
     (customer) => customer.cpf === cpf
-  );
+  ); //retorna true ou false
 
   if (customersAlreadyExists) {
     return response.status(400).json({
       error: "Customer already exists!!",
-    });
+    }); // retornar true ou false
   }
   customers.push({
     cpf,
@@ -28,4 +28,14 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
+app.get("/statement/", (request, response) => {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if(!customer){
+    return response.status(400).json({error: "Customer not found"});
+  }
+  return response.status(201).json(customer.statement);
+});
 app.listen(3333);
