e9687244b2ac1eeadeb296dfeb23594226355b47
José Augusto <joseaugusto0408@gmail.com>
2022-03-02 06:55:58 -0300

    extract list

--- src/index.js
+++ src/index.js
@@ -94,4 +94,16 @@ app.post('/withdraw', verifyIfExistsAccountCPF, (request, response) => {
 	return response.status(201).send();
 });
 
+app.get('/statement/date', verifyIfExistsAccountCPF, (request, response) => {
+	const {customer} = request;
+	const { date } = request.query;
+
+	const dateFormat = new Date(date + ' 00:00');
+
+	const statement = customer.statement.filter((statement) => statement.create_at.toDateString() === new Date(dateFormat).toDateString());
+
+	return response.json(statement);
+});
+
+
 app.listen(3333);
